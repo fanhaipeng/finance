b65cc68d7d5318f37a5e1326b3c13b7440a21403
Haipeng Fan <fanhaipeng@gmail.com>
2010-05-16 16:01:06 +0800

    user unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates_presence_of :nickname, :email, :salt, :password
+  validates_format_of :email, 
+                      :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                      :message => '电子邮件格式不正确'
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,5 +1,29 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def test_presence_of_fields
+    user = User.new    
+    assert !user.valid?
+    assert user.errors.invalid?(:nickname)
+    assert user.errors.invalid?(:email)
+    assert user.errors.invalid?(:salt)
+    assert user.errors.invalid?(:password)
+  end
 
+  def test_email_format
+    user = User.new( :nickname => 'nickname',
+                      :salt => 'salt',
+                      :password => 'password')
+    user.email = 'user'
+    assert !user.valid?
+    assert_equal '电子邮件格式不正确', user.errors.on(:email)
+
+    user.email = 'user@aaa'
+    assert !user.valid?
+    assert_equal '电子邮件格式不正确', user.errors.on(:email)
+
+    user.email = 'user@example.com'
+    assert_equal 1, user.role
+    assert user.valid?
+  end
 end
