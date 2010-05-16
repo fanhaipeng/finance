ef97ee7586a928250e654fe6f98e940be8844729
Haipeng Fan <fanhaipeng@gmail.com>
2010-05-16 16:53:14 +0800

    user model unittests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ActiveRecord::Base
-  validates_presence_of :nickname, :email, :salt, :password
+  validates_presence_of :nickname, :email, :salt, 
+                        :password, :password_confirmation
   validates_format_of :email, 
                       :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
                       :message => '电子邮件格式不正确'
+  attr_accessor :password_confirmation
+  validates_confirmation_of :password,
+                            :message => '密码不一致'
+
+  validate :password_non_blank
+
+private
+  def password_non_blank
+    errors.add_to_base("密码不能为空") if password.blank?
+  end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -13,7 +13,8 @@ class UserTest < ActiveSupport::TestCase
   def test_email_format
     user = User.new( :nickname => 'nickname',
                       :salt => 'salt',
-                      :password => 'password')
+                      :password => 'password',
+                      :password_confirmation => 'password')
     user.email = 'user'
     assert !user.valid?
     assert_equal '电子邮件格式不正确', user.errors.on(:email)
@@ -25,5 +26,22 @@ class UserTest < ActiveSupport::TestCase
     user.email = 'user@example.com'
     assert_equal 1, user.role
     assert user.valid?
+  end
+
+  def test_password_confirmation
+    user = User.new( :nickname => 'nickname',
+                     :salt => 'salt',
+                     :email => 'user@example.com')
+    user.password = 'password'
+    user.password_confirmation = 'pswd'
+    assert !user.valid?
+    assert_equal '密码不一致', user.errors.on(:password)
+
+    user.password_confirmation = 'password'
+    assert user.valid?
+
+  end
+
+  def test_password_non_blank
   end
 end
