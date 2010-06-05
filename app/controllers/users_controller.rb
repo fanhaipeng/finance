51d823bf2d826232960551ede37f4ea37a94e12c
Haipeng Fan <fanhaipeng@gmail.com>
2010-06-05 23:42:13 +0800

    add title to users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   # GET /users.xml
   def index
     @users = User.all
-
+    @page_title = '用户列表'
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @users }
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-
+    @page_title = '用户基本信息'
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @user }
@@ -31,7 +31,7 @@ class UsersController < ApplicationController
   # GET /users/new.xml
   def new
     @user = User.new
-
+    @page_title = '创建新用户'
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @user }
@@ -40,6 +40,7 @@ class UsersController < ApplicationController
 
   # GET /users/1/edit
   def edit
+    @page_title = '更新用户信息'
     @user = User.find(params[:id])
   end
 
@@ -55,6 +56,7 @@ class UsersController < ApplicationController
         format.html { redirect_to(@user) }
         format.xml  { render :xml => @user, :status => :created, :location => @user }
       else
+        @page_title = '创建新用户'
         format.html { render :action => "new" }
         format.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
       end
@@ -71,6 +73,7 @@ class UsersController < ApplicationController
         format.html { redirect_to(@user) }
         format.xml  { head :ok }
       else
+        @page_title = '更新用户信息'
         format.html { render :action => "edit" }
         format.xml  { render :xml => @user.errors, 
                              :status => :unprocessable_entity }
