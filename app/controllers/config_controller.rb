3a268eae47092f9f66d847f325d56c8a4478b89a
Haipeng Fan <haipeng@localhost.(none)>
2010-10-04 14:28:00 +0800

    show expense_types and expense_tags on config page

--- app/controllers/config_controller.rb
+++ app/controllers/config_controller.rb
@@ -2,6 +2,19 @@ class ConfigController < ApplicationController
   before_filter :set_active_tab
 
   def index
+    @expense_types = ExpenseType.find_all_by_user_id(session[:user_id])
+    @expense_tags = ExpenseTag.find_all_by_user_id(session[:user_id])
+  end
+
+  def new_type
+    @expense_type = ExpenseType.new(params[:expense_type])
+    @expense_type.user_id = session[:user_id]
+    respond_to do |format|
+      if !@expense_type.save
+        flash[:notice] = '创建新消费类型失败'
+      end
+      format.html { redirect_to config_action_url }
+    end
   end
 
   private
