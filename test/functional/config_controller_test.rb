a6bafc8f7c12b387fe2a4b703740758f0077fcac
Haipeng Fan <haipeng@localhost.(none)>
2010-10-04 15:34:35 +0800

    add functional tests for config page

--- test/functional/config_controller_test.rb
+++ test/functional/config_controller_test.rb
@@ -1,8 +1,27 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class ConfigControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    session[:user_id] = users(:one).id
+  end
+
+  def test_index
+    get :index
+    assert_response :success
+
+    assert assigns(:expense_types).length >0
+    assert assigns(:expense_tags).length >0
+    assigns(:expense_types).each{|type| assert_equal(1, type.user_id)}
+    assigns(:expense_tags).each{|tag| assert_equal(1, tag.user_id)}
+  end
+
+  def test_new_type
+    expense_type = { :description => '新标签' }
+    assert_difference 'ExpenseType.count' do
+      post :new_type,
+           :expense_type => expense_type
+    end
+
+    assert_redirected_to config_action_url
   end
 end
